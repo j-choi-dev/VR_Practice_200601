a0b3bfc2c2af4f7cddf083b03767a253ee1ee1c3
j_choi <j_choi@vegames.net>
2020-07-11 11:44:50 +0900

    分類前

---
+++ Assets/CameraModeInfo.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+namespace Choi.VR2020.Domain.System
+{
+    public class DeviceOrientationInfo
+    {
+        public DeviceOrientation Value { get; private set; }
+
+        public DeviceOrientation SetValue(DeviceOrientation value)
+        {
+            Value = value;
+            return value;
+        }
+    }
+    public class CameraModeInfo
+    {
+        [SerializeField]
+        private bool m_isVirtualMode;
+
+        [SerializeField]
+        public CameraState State { get; private set; } = CameraState.NONE;
+
+        public CameraModeInfo()
+        {
+            XRSettings.enabled = false;
+            State = CameraState.Normal;
+            SetVRSettings();
+        }
+
+        /// <summary>
+        /// Set Normal
+        /// </summary>
+        /// <returns>ステート</returns>
+        public CameraState SetState(CameraState state)
+        {
+            State = state;
+            SetVRSettings();
+            return State;
+        }
+
+        private bool SetVRSettings()
+        {
+            XRSettings.enabled = State != CameraState.NONE
+                        || State != CameraState.Normal;
+            return true;
+        }
+    }
+
+    public enum CameraState
+    {
+        NONE,
+        Normal,
+        ChangeToVirtual,
+        Virtual,
+        ChangeToNormal,
+    }
+}
---
+++ Assets/ChangeModeImageControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using Choi.VR2020.Domain.System;
+
+namespace Choi.VR2020.UI.System
+{
+    public class ChangeModeImageControl : MonoBehaviour
+    {
+        [SerializeField] private Sprite[] m_guideImage;
+        [SerializeField] private SimpleImage m_image;
+        [SerializeField] private SimpleText m_text;
+
+        private void Awake()
+        {
+        }
+
+        private async void Start()
+        {
+            m_image.gameObject.SetActive(false);
+            m_text.gameObject.SetActive(false);
+            await ChangeModeToVirtual();
+        }
+
+        public async UniTask ChangeModeToVirtual()
+        {
+            m_image.gameObject.SetActive(true);
+            if (Manager.Instance.CameraMode.State != CameraState.NONE
+                && Manager.Instance.CameraMode.State != CameraState.Normal)
+            {
+                Debug.Log($"Already CameraMode : {Manager.Instance.CameraMode.State}");
+                return ;
+            }
+            var orientResult = await DoCheckLandScape();
+            m_image.gameObject.SetActive(false);
+            Manager.Instance.CameraMode.SetState(orientResult);
+
+            m_text.gameObject.SetActive(true);
+            var countDownResult = await DoCountDown(true);
+            m_text.gameObject.SetActive(false);
+            Manager.Instance.CameraMode.SetState(countDownResult);
+        }
+
+        private async UniTask<CameraState> DoCheckLandScape()
+        {
+            var timer = 0f;
+            Debug.Log($"nowOrientation DoCheckLandScape : {Manager.Instance.Orientation.Value }");
+            while (Manager.Instance.Orientation.Value != DeviceOrientation.LandscapeLeft)
+            {
+                if (Manager.Instance.Orientation.Value != Input.deviceOrientation)
+                {
+                    Manager.Instance.Orientation.SetValue(DeviceOrientation.LandscapeLeft);
+                }
+                timer = (int)timer < m_guideImage.Length - 1 ? timer += Time.deltaTime * 3 : 0;
+                await UniTask.Yield(PlayerLoopTiming.FixedUpdate);
+                m_image.Set(m_guideImage[(int)timer]);
+            }
+            return CameraState.ChangeToVirtual;
+        }
+
+        private async UniTask<CameraState> DoCountDown(bool isToVirtual)
+        {
+            var timer = 10f;
+            m_text.Set((int)timer);
+            while (timer > 0f)
+            {
+                m_text.Set((int)timer);
+                Debug.Log($"countTimer : {timer}");
+                // await UniTask.Yield(PlayerLoopTiming.FixedUpdate);
+                await UniTask.Delay(1000);
+                timer -= 1f;
+            }
+            return isToVirtual ? CameraState.Virtual : CameraState.Normal;
+        }
+    }
+}
