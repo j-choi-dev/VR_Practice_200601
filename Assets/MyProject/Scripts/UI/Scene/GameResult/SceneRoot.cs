3497946c8d70742b4779305bd4ad38bcf61de8d5
j_choi <j_choi@vegames.net>
2020-10-22 17:57:20 +0900

    シーン配置

---
+++ Assets/MyProject/Scripts/UI/Scene/GameResult/SceneRoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using Choi.MyProj.UI.InGame;
+
+namespace Choi.MyProj.UI.Scene.GameResult
+{
+    public sealed class SceneRoot : SceneRootBase
+    {
+
+        /// <summary>
+        /// Start
+        /// </summary>
+        private async void Start()
+        {
+            await Init();
+        }
+
+        /// <summary>
+        /// シーンチェンジメソッド
+        /// </summary>
+        public override void SceneChangeToNext()
+        {
+        }
+
+        /// <summary>
+        /// Init
+        /// </summary>
+        /// <returns>Initialize Result</returns>
+        public override async UniTask<bool> Init()
+        {
+            return true;
+        }
+    }
+}
